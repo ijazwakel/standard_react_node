e875e5917ebb774024ce30560a02f68bf78768dd
Ijaz Wakeel <ijazwakeel.dev@gmail.com>
2024-10-26 01:00:20 +0500

    new -update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: "src",
-  envDir: "../",
-  envPrefix: "PAYPAL",
+  root: 'src', // This should point to the directory containing index.html
+  envDir: '../',
+  envPrefix: 'PAYPAL',
   server: {
     port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:8080",
+      '/api': {
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+  build: {
+    outDir: './dist', // Output will be in the root `dist`
+    rollupOptions: {
+      // Optional: specify any external modules here if needed
+    },
+  },
+});
